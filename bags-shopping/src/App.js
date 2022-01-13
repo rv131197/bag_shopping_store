fb27834cacabf923e60dc56af1ede29948e7aaa9
Ramya V <ramya@Ramyas-MacBook-Air.local>
2022-01-13 21:14:37 +0530

    adding header and styling

--- bags-shopping/src/App.js
+++ bags-shopping/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
+import Header from './components/components/header/header.component';
 import { HomePage } from './components/pages/homePage/homepage.component';
 import ShopPage from './components/pages/shop/shop.component';
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
       <Route exact path="/" component={HomePage} />
      <Route exact path="/shop" component={ShopPage} />
