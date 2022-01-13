539648ca3e1b897c7f1aafddb29be332a65a5bc4
Ramya V <ramya@Ramyas-MacBook-Air.local>
2022-01-13 17:33:54 +0530

    added basic routing

--- bags-shopping/src/components/components/menu-item/menu-item.component.jsx
+++ bags-shopping/src/components/components/menu-item/menu-item.component.jsx
@@ -1,8 +1,11 @@
 import React from "react";
+import { withRouter } from "react-router";
 import './menu-item.styles.scss';
 
-const MenuItem = ({title, id, imageUrl, size}) => (
-    <div className={`${size} menu-item`}>
+const MenuItem = ({title, id, imageUrl, size, history, linkUrl, match}) => (
+    <div className={`${size} menu-item`}
+    onClick={() => history.push(`${match.url}${linkUrl}`)}>
+       {console.log(`${match.url}${linkUrl}`)}
         <div className='background-image' style={{
         backgroundImage: `url(${imageUrl})`
     }} />
@@ -13,4 +16,4 @@ const MenuItem = ({title, id, imageUrl, size}) => (
     </div>
 )
 
-export default MenuItem;
+export default withRouter(MenuItem);
