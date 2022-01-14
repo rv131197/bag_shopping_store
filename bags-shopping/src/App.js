dd2a3359960648cc12961ad310ddc3df090cd399
Ramya V <ramya@Ramyas-MacBook-Air.local>
2022-01-14 20:31:35 +0530

    Added sign features with firebase

--- bags-shopping/src/App.js
+++ bags-shopping/src/App.js
@@ -5,18 +5,42 @@ import Header from './components/components/header/header.component';
 import { HomePage } from './components/pages/homePage/homepage.component';
 import ShopPage from './components/pages/shop/shop.component';
 import SignInSignUpPage from './components/pages/sign-in-sign-up/sign-in-sign-up.component';
+import {auth} from './firebase/firebase.utils';
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Switch>
-      <Route exact path="/" component={HomePage} />
-      <Route exact path="/shop" component={ShopPage} />
-      <Route exact path="/signin" component={SignInSignUpPage} />
-     </Switch>
-    </div>
-  );
+class App extends React.Component{
+  constructor(){
+    super();
+    this.state={
+      currentUser: null
+    }
+  }
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount(){
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => { // takes a ftn where the parameter is what the user state is of the auth on our firebase project
+      this.setState({currentUser: user});
+
+      console.log(user)
+    }) 
+  }
+
+  componentWillUnmount(){
+    this.unsubscribeFromAuth(); // this will close the subscription
+  }
+
+  render(){
+    return (
+      <div>
+        <Header currentUser={this.state.currentUser} />
+        <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/shop" component={ShopPage} />
+        <Route exact path="/signin" component={SignInSignUpPage} />
+       </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
--- bags-shopping/src/firebase/firebase.utils.js
+++ bags-shopping/src/firebase/firebase.utils.js
@@ -1,2 +1,27 @@
-import firebase from 'firebase/app';
-import 'firebase/firestore';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth';
+
+const config = {
+        apiKey: "AIzaSyAh90TrFVbga0BMhl-lJ89YTLQIMBJhUd4",
+        authDomain: "shopping-db-54a25.firebaseapp.com",
+        projectId: "shopping-db-54a25",
+        storageBucket: "shopping-db-54a25.appspot.com",
+        messagingSenderId: "778852378630",
+        appId: "1:778852378630:web:d251580d7f77f9c3d37b87",
+        measurementId: "G-17097GC753"
+}  // this is the config object from firebase
+
+firebase.initializeApp(config); // initializing our app with the above config
+
+export const auth = firebase.auth(); // accessing auth() method on firebase, anything related to authentication
+export const firestore = firebase.firestore();
+
+
+// setting up google authentication utlility
+const provider = new firebase.auth.GoogleAuthProvider() // gives access to googleAuthProvider class from auth library
+provider.setCustomParameters({ prompt: 'select_account'}) //trigger google pop up whenever we use this for sign in
+
+export const signInWithGoogle = () => auth.signInWithPopup(provider) // takes the provider class we made. but it takes from many types of popups like twitter etc.
+
+export default firebase; // exporting it incase we want the whole library
