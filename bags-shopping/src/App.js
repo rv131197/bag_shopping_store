6d5ab61022247766061d24355934236dcc06e459
Ramya V <ramya@Ramyas-MacBook-Air.local>
2022-01-14 13:14:06 +0530

    Added custom button components'

--- bags-shopping/src/App.js
+++ bags-shopping/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Header from './components/components/header/header.component';
 import { HomePage } from './components/pages/homePage/homepage.component';
 import ShopPage from './components/pages/shop/shop.component';
+import SignInSignUpPage from './components/pages/sign-in-sign-up/sign-in-sign-up.component';
 
 function App() {
   return (
@@ -11,7 +12,8 @@ function App() {
       <Header />
       <Switch>
       <Route exact path="/" component={HomePage} />
-     <Route exact path="/shop" component={ShopPage} />
+      <Route exact path="/shop" component={ShopPage} />
+      <Route exact path="/signin" component={SignInSignUpPage} />
      </Switch>
     </div>
   );
