530c147c4215339fa3f71f7998b2ed3223528d7b
Ramya V <ramya@Ramyas-MacBook-Air.local>
2022-01-23 18:25:46 +0530

    added additem and removeitem to checkout component

--- bags-shopping/src/redux/cart/cart.utils.js
+++ bags-shopping/src/redux/cart/cart.utils.js
@@ -12,3 +12,17 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
 
         return [...cartItems, {...cartItemToAdd, quantity: 1}] // quantity property gets attached the first time around since this if block won't run if it's a new item!
 }
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+    const existingCartItem = cartItems.find(
+        cartItem => cartItem.id === cartItemToRemove.id);
+
+    if(existingCartItem.quantity === 1){
+        return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id)
+    }
+
+    return cartItems.map(cartItem => 
+        cartItem.id === cartItemToRemove.id ? 
+        {...cartItem, quantity: cartItem.quantity-1} : 
+        cartItem)
+}
